3c8eb598602923c30dda515a11351d5b555a6101
jtoma95 <jtoma@kth.se>
2021-02-18 14:55:49 +0100

    2nd commit

---
+++ hk_ros_2021/scripts/listen.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+import rospy 
+import yaml
+import rospkg
+
+from std_msgs.msg import String
+
+
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    
+def listener():
+
+    # In ROS, nodes are uniquely named. If two nodes with the same
+    # name are launched, the previous one is kicked off. The
+    # anonymous=True flag means that rospy will choose a unique
+    # name for our 'listener' node so that multiple listeners can
+    # run simultaneously.
+    rospy.init_node('listen', anonymous=True)
+
+    rospy.Subscriber("tag_detections", String, callback)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+
+object_list = []
+
+object_list.append( {"obj_type": "A", "XY_pos": [10.756,32.332]} )
+object_list.append( {"obj_type": String, "XY_pos": [10.756,32.332]} )
+
+filename = "latest_output_file.yaml"
+filepath = rospkg.RosPack().get_path('hk_ros_2021') + '/exported_detection_logs/'
+
+with open(filepath + filename, 'w') as outfile:
+    yaml.dump_all(object_list, outfile,explicit_start=True)
+
+if __name__ == '__main__':
+    listener()
--- hk_ros_2021/scripts/output_generation_template.py
+++ hk_ros_2021/scripts/output_generation_template.py
@@ -5,24 +5,30 @@
 import yaml
 import rospkg
 
-# 1 create an empty list to store the detections
-detections = []
+#class object
+#	def __init__(self, obj_type = None, XY_pos = None)
+#	self.obj_type = obj_type
+#	self.XY_pos = XY_pos 
+
+
+# 1 create an empty list to store the object_list
+object_list = []
 
 # 2 append detections during the run
 # remember to add logic to avoid duplicates
 
 # first dummy detection (apriltag)
-detections.append({"obj_type": "A", "XY_pos": [0.756,3.332]})
+object_list.append( {"obj_type": "A", "XY_pos": [0.756,3.332]} )
 
 # second dummy detection (geometric shape)
-detections.append({"obj_type": "B", "XY_pos": [3.396,0.123]})
+object_list.append({"obj_type": "B", "XY_pos": [3.396,0.123]})
 
 # third dummy detection (animal)
-detections.append({"obj_type": "C", "XY_pos": [6.001,2.987]})   
-    
+object_list.append({"obj_type": "C", "XY_pos": [6.001,2.987]})
+
 # 3 save the file
 filename = "latest_output_file.yaml"
-filepath = rospkg.RosPack().get_path('hk_ros_2021') + '/exported_detection_logs/' 
+filepath = rospkg.RosPack().get_path('hk_ros_2021') + '/exported_detection_logs/'
 
 with open(filepath + filename, 'w') as outfile:
-    yaml.dump_all(detections, outfile,explicit_start=True)
+    yaml.dump_all(object_list, outfile,explicit_start=True)
