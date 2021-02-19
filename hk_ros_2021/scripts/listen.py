8e387c61f9c55d2276d26e90e7c6379f2c5bbdb3
jtoma95 <jtoma@kth.se>
2021-02-19 12:01:08 +0100

    små grejjer

--- hk_ros_2021/scripts/listen.py
+++ hk_ros_2021/scripts/listen.py
@@ -3,13 +3,13 @@ import rospy
 import yaml
 import rospkg
 
-from std_msgs.msg import String
+#from std_msgs.msg import String
 
 
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
     
-def listener():
+def listen():
 
     # In ROS, nodes are uniquely named. If two nodes with the same
     # name are launched, the previous one is kicked off. The
@@ -18,7 +18,7 @@ def listener():
     # run simultaneously.
     rospy.init_node('listen', anonymous=True)
 
-    rospy.Subscriber("tag_detections", String, callback)
+    rospy.Subscriber("/tag_detections", AprilTagDetectionArray, callback)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
@@ -26,8 +26,9 @@ def listener():
 
 object_list = []
 
-object_list.append( {"obj_type": "A", "XY_pos": [10.756,32.332]} )
-object_list.append( {"obj_type": String, "XY_pos": [10.756,32.332]} )
+object_list.append( {"obj_type": "A", "XY_pos": [30.756,32.332]} )
+object_list.append( {"obj_type": "A", "XY_pos": [20.756,39.332]} )
+object_list.append( {"obj_type": "A", "XY_pos": AprilTagDetectionArray[0] } )
 
 filename = "latest_output_file.yaml"
 filepath = rospkg.RosPack().get_path('hk_ros_2021') + '/exported_detection_logs/'
@@ -36,4 +37,4 @@ with open(filepath + filename, 'w') as outfile:
     yaml.dump_all(object_list, outfile,explicit_start=True)
 
 if __name__ == '__main__':
-    listener()
+    listen()
